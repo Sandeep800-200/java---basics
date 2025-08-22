83a73efc3965bb09c035e48f9167196ae8a164ea
SANDEEP R <sandeepravikumaran@gmail.com>
2025-08-22 23:12:29 +0530

    Add files via upload
    Add Java programs: reverse string, sum of n numbers, min in array

---
+++ Sumn.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Sumn 
+{
+public static int sum(int n)
+{
+    Scanner sc = new Scanner (System.in);
+    int s= 0;
+    for(int i=1 ; i<=n; i++)
+        {
+            
+            System.out.println("enter thr number to be added :- ");
+            int e = sc.nextInt();
+            s=s+e;
+        
+        }
+        return s;
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("enter the number of natural number ");
+        int n = sc.nextInt();
+        int s = sum(n);
+System.out.println("sum of "+ n + "natural number is "+ s);
+
+    }
+}
+
+
