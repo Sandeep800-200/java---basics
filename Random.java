0c27499b0f0ba0246fa12e9266c4d955e3ca8ddf
SANDEEP R <sandeepravikumaran@gmail.com>
2025-08-27 21:18:01 +0530

    Add files via upload

---
+++ Random.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Random
+{
+    public static void main (String [] args )
+    {
+    Scanner sc = new Scanner (System.in);
+    System.out.println("hi");
+    sc.close();
+    }
+}
