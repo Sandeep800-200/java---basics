50e879ae9286936dc5563078c02a433c187d828a
SANDEEP R <sandeepravikumaran@gmail.com>
2025-08-28 23:43:10 +0530

    Add files via upload

--- Armin.java
+++ Armin.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
-public class Armin {
-
-    // Method to input array
+public class Armin 
+{
     public static int[] irr(int n, Scanner sc) {
-        int[] arr = new int[n]; // size = n
+        int[] arr = new int[n]; 
         for (int i = 0; i < n; i++) {
             System.out.print("Enter element " + (i + 1) + ": ");
             arr[i] = sc.nextInt();
@@ -12,7 +11,6 @@ public class Armin {
         return arr;
     }
 
-    
     public static int mn(int[] arr) {
         int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
@@ -24,6 +22,28 @@ public class Armin {
         return min;
     }
 
+    public static int mX(int[] arr) {
+        int max  = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max)
+             {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+     
+    public static int sum(int[] arr) {
+        int s = 0;
+         for(int i = 1; i < arr.length; i++)
+        {
+            s=s+arr[i];
+        }
+        return s;
+
+    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -32,12 +52,17 @@ public class Armin {
 
         int[] ar = irr(n, sc);
         int minimum = mn(ar);
+        int maximum = mX(ar);
+        int sum = sum(ar);
 
         System.out.println("The lowest number in array is: " + minimum);
+        System.out.println("The highest number in array is: " + maximum);
+        System.out.println("The sum of  array is: " +sum );
 
         sc.close();
     }
 }
+
 
    
 
